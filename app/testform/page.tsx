2a5fd2d43607debee79e65a16a8e6ba0ee433a83
vasanth0017 <eswarvasanth17@gmail.com>
2025-03-04 20:07:17 +0530

    return testform issue

--- app/testform/page.tsx
+++ app/testform/page.tsx
@@ -1,11 +1,11 @@
-import BasicForm from '@/components/form';
-import react from 'react';
+import BasicForm from "@/components/form";
+import react from "react";
 import { Suspense } from "react";
 
 export default async function Page() {
-    
-
+  return (
     <Suspense fallback={<div>Loading...</div>}>
       <BasicForm />
     </Suspense>
+  );
 }
--- components/qr-code.tsx
+++ components/qr-code.tsx
@@ -8,7 +8,7 @@ export default function GenerateQR() {
   const [uniqueId, setUniqueId] = useState(() => Date.now().toString());
  const code = "9469d599-7a58-469c-b6cf-733b112ec80b"
 const qrData = `${process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'}/testform/?code=${code}`;
-
+console.log(qrData)
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>QR Code for Paint Box</h2>
